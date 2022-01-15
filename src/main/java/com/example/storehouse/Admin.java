163bd0313854845793beeb3e9dec6a1d37c9a978
Svyatoslav <43129468+Svyatoslave@users.noreply.github.com>
2022-01-16 01:02:28 +0300

    Update Admin.java

--- src/main/java/com/example/storehouse/Admin.java
+++ src/main/java/com/example/storehouse/Admin.java
@@ -13,8 +13,8 @@ public class Admin {
         Database.productList.remove(Database.getProductByID(id));
     }
 	 public static void checkProduct(int id, String nameLogin,String direction,int quantity, String naming, String dating ){
-		 Products product=Database.getProductByName(naming)
-		  Client client=Database.getClientByName(nameLogin)
+		 Products product=Database.getProductByName(naming);
+		  Client client=Database.getClientByName(nameLogin);
 		Order order = new Order(dating, product, quantity, direction, client);
         Database.orderList.add(order);
     }
